ed5f35bd8d4de541b265215dfcdcec7fd862b976
Morozov Konstantin <morozov.cookie@gmail.com>
2019-03-19 17:24:56 +0300

    support POST HTTP method

--- flask_gunicorn_docker/api/users.py
+++ flask_gunicorn_docker/api/users.py
@@ -6,3 +6,8 @@ users_api_bp = Blueprint(name='user_api', import_name=__name__)
 @users_api_bp.route(rule='/', methods=['GET'])
 def get_users_list():
     return jsonify([])
+
+
+@users_api_bp.route(rule='/', methods=['POST'])
+def store_user():
+    return '', 201
