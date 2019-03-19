033aa153fbdc8ca01210e0ace8f8ca7f83e84d1d
Morozov Konstantin <morozov.cookie@gmail.com>
2019-03-19 20:33:16 +0300

    correct way to usage GET /users

--- flask_gunicorn_docker/api/users.py
+++ flask_gunicorn_docker/api/users.py
@@ -1,4 +1,5 @@
-from flask import jsonify
+from flask import jsonify, request
+from typing import Tuple
 
 from flask_gunicorn_docker import storage
 from flask_gunicorn_docker.api import users_api_bp
@@ -12,16 +13,19 @@ store_user_use_case = StoreUser(repository=user_repository)
 
 
 @users_api_bp.route(rule='/', methods=['GET'])
-def get_users_list():
+def get_users_list() -> Tuple[str, int]:
     try:
-        result = user_list_use_case.do()
+        result = user_list_use_case.do(
+            limit=request.args.get('limit', 100),
+            offset=request.args.get('offset', 0)
+        )
         return jsonify(result), 200
     except Exception as e:
         return jsonify({'message': e.__str__()}), 500
 
 
 @users_api_bp.route(rule='/', methods=['POST'])
-def store_user():
+def store_user() -> Tuple[str, int]:
     try:
         store_user_use_case.do(user=None)
     except Exception as e:
