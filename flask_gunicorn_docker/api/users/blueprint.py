f9c4e3374e856cc1d7c45a1b9b35e7206a9fef61
Morozov Konstantin <morozov.cookie@gmail.com>
2019-03-20 10:49:41 +0300

    rework api structure

---
+++ flask_gunicorn_docker/api/users/blueprint.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+users_api_bp = Blueprint(
+    name='user_api',
+    import_name=__name__
+)
--- flask_gunicorn_docker/api/users/views.py
+++ flask_gunicorn_docker/api/users/views.py
@@ -1,15 +1,10 @@
-from flask import Blueprint, jsonify, request
+from flask import jsonify, request
 from typing import Tuple
 
+from .blueprint import users_api_bp
 from flask_gunicorn_docker import storage
 from flask_gunicorn_docker.repositories import UserRepository
 from flask_gunicorn_docker.usecases import UsersList, StoreUser
-
-
-users_api_bp = Blueprint(
-    name='user_api',
-    import_name=__name__
-)
 
 user_repository = UserRepository(storage=storage)
 
